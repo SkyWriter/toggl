bda15c863e2f0c5d89a9bbc97811c09ab34d4259
Ivan Kasatenko <sky.31338@gmail.com>
2013-07-24 19:57:41 +0400

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :toggl do
+  name 'Toggl plugin'
+  author 'Ivan Kasatenko <sky@uniqsystems.ru>'
+  description 'Plugin to support Toggl Redmine integration.'
+  version '0.0.1'
+  url 'http://github.com/skywriter/toggl'
+  author_url 'http://uniqsystems.ru/'
+end
