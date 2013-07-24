91b6050867f138fc13a7a3492d3ff4db8af7cd45
Ivan Kasatenko <sky.31338@gmail.com>
2013-07-24 19:59:50 +0400

    Added toggle time entries model

---
+++ app/models/toggl_time_entry.rb
@@ -0,0 +1,3 @@
+class TogglTimeEntry < ActiveRecord::Base
+  unloadable
+end
---
+++ db/migrate/001_create_toggl_time_entries.rb
@@ -0,0 +1,6 @@
+class CreateTogglTimeEntries < ActiveRecord::Migration
+  def change
+    create_table :toggl_time_entries do |t|
+    end
+  end
+end
