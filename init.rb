fd6e49f34db50cf5379afdf7fce6d237402ad77a
Ivan Kasatenko <sky.31338@gmail.com>
2014-04-01 09:57:28 +0200

    Version bump

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :toggl do
   name 'Toggl plugin'
   author 'Ivan Kasatenko <sky@uniqsystems.ru>'
   description 'Plugin to support Toggl Redmine integration.'
-  version '0.0.2'
+  version '0.0.3'
   url 'http://github.com/skywriter/toggl'
   author_url 'http://uniqsystems.ru/'
 end
