6df2cf7cbc847cdf949f1d12c89bff3647f5c099
enter <enter@lap.lap>
2014-09-11 17:05:54 +0200

    Fix migration not creating tables

--- db/migrate/001_create_toggl_time_entries.rb
+++ db/migrate/001_create_toggl_time_entries.rb
@@ -1,6 +1,9 @@
 class CreateTogglTimeEntries < ActiveRecord::Migration
   def change
     create_table :toggl_time_entries do |t|
+      t.column :id, :integer
     end
+
+    add_index :toggl_time_entries, [:id], :name => "id"
   end
 end
